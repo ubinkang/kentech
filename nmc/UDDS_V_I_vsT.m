83abb344bf9ebac6b622a328f653dd5e3bbba8e1
ubinkang <kangubin@gmail.com>
2025-01-08 15:38:25 +0900

    edit
    edit

--- nmc/UDDS_V_I_vsT.m
+++ nmc/UDDS_V_I_vsT.m
@@ -2,7 +2,7 @@
 fileName = '03-21-17_00.29 25degC_UDDS_Pan18650PF.mat';
 data = load(fileName);
 
-% Time, Voltage, Current 데이터 추출
+% 데이터 추출
 time = data.meas.Time;       % 시간 데이터
 voltage = data.meas.Voltage; % 전압 데이터
 current = data.meas.Current; % 전류 데이터
@@ -13,22 +13,22 @@ figure;
 % 왼쪽 y축: 전류
 yyaxis left;
 plot(time, current, 'LineWidth', 0.5, 'Color', 'r'); % 전류 플롯
-ylabel('Current (A)'); % 왼쪽 y축 레이블
-ylim([min(current) max(current)]); % y축 범위 설정
-grid on;
+ylabel('Current (A)', 'Color', 'r'); % 왼쪽 y축 레이블 (빨간색)
+set(gca, 'YColor', 'r'); % 왼쪽 y축 눈금 색상을 빨간색으로 설정
+grid on; % 격자 추가
 
 % 오른쪽 y축: 전압
 yyaxis right;
 plot(time, voltage, 'LineWidth', 0.5, 'Color', 'b'); % 전압 플롯
-ylabel('Voltage (V)'); % 오른쪽 y축 레이블
-ylim([min(voltage) max(voltage)]); % y축 범위 설정
+ylabel('Voltage (V)', 'Color', 'b'); % 오른쪽 y축 레이블 (파란색)
+set(gca, 'YColor', 'b'); % 오른쪽 y축 눈금 색상을 파란색으로 설정
 
 % 공통 x축 레이블 및 제목
 xlabel('Time (s)');
-title('Voltage and Current vs Time');
+title('Voltage and Current vs Time (UDDS)');
 legend('Current (A)', 'Voltage (V)', 'Location', 'best');
 
 % 그래프 저장
-outputFileName = 'Voltage_and_Current_with_Dual_YAxis_UDDS.png';
+outputFileName = 'Voltage_and_Current_with_Colored_YAxis_UDDS.png';
 saveas(gcf, outputFileName);
-disp(['전압 및 전류 (이중 y축) vs 시간 그래프가 저장되었습니다: ', outputFileName]);
+disp(['전압 및 전류 (색상 통일) vs 시간 그래프가 저장되었습니다: ', outputFileName]);
